1d1b6d043c03d0c4c14260fe32bda68e505eb14f
wegar2 <awegrzyn17@gmail.com>
2026-01-03 21:02:43 +0100

    subpackage with examples of interacting with files

---
+++ pycs/osapi/02_read_text_from_textfile.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+if __name__ == "__main__":
+
+    fpath = Path(__file__).parent.parent / "data" / "peloponnesian_war.txt"
+
+    # (1) read the whole text at once
+    with open(fpath, "rt") as f:
+        print(f.read())
+
+    # (2) read the text line by line -- using readline()
+    with open(fpath, "rt") as f:
+        while line := f.readline():
+            print(line)
+
+    # (3) read the text line by line -- but load it into list of strings first
+    with open(fpath, "rt") as f:
+        lines: list[str] = f.readlines()
+        for line in lines:
+            print(line)
+            

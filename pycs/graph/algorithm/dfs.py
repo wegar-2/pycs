ce94e8d55d26a1dc841c20a0c0b9295558bf660b
wegar2 <awegrzyn17@gmail.com>
2026-01-11 22:50:45 +0100

    run first quick manual test of DFS routine

--- pycs/graph/algorithm/dfs.py
+++ pycs/graph/algorithm/dfs.py
@@ -4,10 +4,16 @@ from pycs.graph.graph import Graph
 from pycs.graph.vertex import Vertex
 
 
-def dfs(graph: Graph, sv: Vertex, visited: Optional[list[Vertex]] = None):
-
+def dfs(
+        graph: Graph,
+        sv: Vertex,
+        visited: Optional[list[Vertex]] = None,
+        parent_vertex_map: Optional[dict[Vertex, Vertex]] = None
+):
     if visited is None:
         visited: list[Vertex] = [sv]
+    if parent_vertex_map is None:
+        parent_vertex_map: dict[Vertex, Vertex | None] = {sv: None}
 
     if sv not in graph.vertices():
         raise ValueError(
@@ -15,7 +21,6 @@ def dfs(graph: Graph, sv: Vertex, visited: Optional[list[Vertex]] = None):
             f"of the graph! "
         )
 
-    parent_vertex_map: dict[Vertex, Vertex | None] = {sv: None}
     next_vertices: list[Vertex] = [
         e.destination for e in graph.outgoing_incident_edges(sv)
     ]
@@ -24,7 +29,12 @@ def dfs(graph: Graph, sv: Vertex, visited: Optional[list[Vertex]] = None):
         if nv not in visited:
             visited.append(nv)
             parent_vertex_map[nv] = sv
-            res = dfs(graph, sv=nv, visited=visited)
+            res = dfs(
+                graph=graph,
+                sv=nv,
+                visited=visited,
+                parent_vertex_map=parent_vertex_map
+            )
             parent_vertex_map = res["parent_vertex_map"]
             visited = res["visited"]
 
@@ -32,3 +42,14 @@ def dfs(graph: Graph, sv: Vertex, visited: Optional[list[Vertex]] = None):
         "parent_vertex_map": parent_vertex_map,
         "visited": visited
     }
+
+
+if __name__ == "__main__":
+    from pycs.graph.adjacency_map_graph import AdjacencyMapGraph
+    from tests.graph.common import populate_graph_for_search
+
+    graph = populate_graph_for_search(graph=AdjacencyMapGraph())
+
+    res = dfs(graph=graph, sv=Vertex("A"))
+
+    print("Halt! ")
--- tests/graph/common.py
+++ tests/graph/common.py
@@ -4,6 +4,7 @@ from pycs.graph.vertex import Vertex
 
 __all__ = [
     "populate_empty_graph",
+    "populate_graph_for_search",
     "make_vertices",
     "make_edges"
 ]
@@ -39,10 +40,25 @@ def populate_empty_graph(graph: Graph) -> Graph:
     return graph
 
 
+def populate_graph_for_search(graph: Graph) -> Graph:
+    vertices: list[Vertex] = make_vertices()
+    A, B, C, D, E, F, G = vertices
+    for v in vertices:
+        graph.insert_vertex(v)
+
+    graph.insert_edge(A, B)
+    graph.insert_edge(B, C)
+    graph.insert_edge(C, D)
+    graph.insert_edge(D, E)
+    graph.insert_edge(D, F)
+    graph.insert_edge(E, G)
+
+    return graph
+
+
 if __name__ == "__main__":
     from pycs.graph.adjacency_map_graph import AdjacencyMapGraph
     from pycs.graph.adjacency_list_graph import AdjacencyListGraph
     alg = populate_empty_graph(graph=AdjacencyListGraph())
     e = alg.get_edge(Vertex("A"), Vertex("B"))
-    # e == Edge(Vertex("A"), Vertex("B"))
     print("halt")

1ac29e0203c8f8e50a8e892b61fd6af87be121b5
wegar2 <awegrzyn17@gmail.com>
2026-01-21 23:08:41 +0100

    added function for converting single symbols to bitarrays

--- pycs/bybi/src.py
+++ pycs/bybi/src.py
@@ -10,14 +10,21 @@ __all__ = [
 ]
 
 
+def symbol_to_bitarray(l: str, encoding: Encoding) -> bitarray:
+    encoding_size: int = ENCODING_TO_MAX_BITS_PER_CHARACTER[encoding]
+    l_bytes: bytes = l.encode(encoding)
+    ba: bitarray = bitarray([0] * len(l_bytes) * 8)
+    for i, byte_ in enumerate(l_bytes):
+        ba[8*i:8*(i+1)] = bitarray(f"{byte_:08b}")
+    return bitarray([0] * (encoding_size - 8 * len(l_bytes)) + list(ba))
+
+
 def encode_str_to_bitarray(s: str, encoding: Encoding) -> bitarray:
-    s_enc: bytes = s.encode(encoding=encoding)
     bits_per_character: int = ENCODING_TO_MAX_BITS_PER_CHARACTER[encoding]
     ba: bitarray = bitarray(bits_per_character*len(s))
-    for i, byte in enumerate(s_enc):
-        ba[bits_per_character*i:bits_per_character*(i+1)] = bitarray(f"{byte:08b}")
-
-
+    for i in range(len(s)):
+        ba[bits_per_character*i:bits_per_character*(i+1)] = (
+            symbol_to_bitarray(l=s[i], encoding=encoding))
     return ba
 
 

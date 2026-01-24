2f2eaa4017e755e23b6f70fc640c9afc3ddc2c22
wegar2 <awegrzyn17@gmail.com>
2026-01-24 20:29:32 +0100

    added function for extracting i-th last remainder

--- pycs/algorithms/sorting/radix_sort.py
+++ pycs/algorithms/sorting/radix_sort.py
@@ -1,24 +1,37 @@
-# from array import array
+from array import array
 
 
 def _get_digit_at_base(n: int, radix: int = 2):
     pass
 
 
-# def _counting_sort(nums: list[int], digit: int) -> array:
-#
-#     ar: array = array("i", nums)
-#     ar_max: int = max(ar)
-#
-#     sorted_ar: array = array("i", [0] * len(ar))
-#     counts_ar: array = array("i", range(ar_max + 1))
-#
-#     digits_ar: array = array("i", ar)
-#
-#     # for i
-#
-#     return sorted_ar
+def _counting_sort(nums: list[int], digit: int) -> array:
+
+    ar: array = array("i", nums)
+    ar_max: int = max(ar)
+
+    sorted_ar: array = array("i", [0] * len(ar))
+    counts_ar: array = array("i", range(ar_max + 1))
+
+    digits_ar: array = array("i", ar)
+
+    # for i
+
+    return sorted_ar
+
+
+def _matrix_based_radix_sort(nums: list[int]) -> list[int]:
+    pass
+
+
+def _radix_sort(nums: list[int]) -> list[int]:
+    pass
 
 
 def radix_sort(nums: list[int]) -> list[int]:
-    pass
+
+    base10_max_len: int = max([len(str(x)) for x in nums])
+    str_nums: list[str] = [str(x).rjust(base10_max_len, "0") for x in nums]
+
+
+    return nums
--- pycs/nt/common.py
+++ pycs/nt/common.py
@@ -5,8 +5,11 @@ from pycs.common.types import Radix
 
 __all__ = [
     "get_nn_len_at_base",
+    "get_ith_last_digit_of_number_repr",
     "to_base"
 ]
+
+_GET_NN_LEN_AT_BASE: dict[tuple[int, Radix], int] = {}
 
 
 def _validate_is_nn(n: int) -> None:
@@ -19,20 +22,38 @@ def _validate_radix(r: Radix) -> None:
         raise ValueError(f"Radix {r=} is not allowed! ")
 
 
-def get_nn_len_at_base(n: int, radix: Radix = 2) -> int:
-    return floor(log(n, radix)) + 1
+def get_nn_len_at_base(
+        n: int,
+        radix: Radix = 2,
+        cache_call: bool = False
+) -> int:
+    if (n, radix) in _GET_NN_LEN_AT_BASE:
+        return _GET_NN_LEN_AT_BASE[(n, radix)]
+    res = floor(log(n, radix)) + 1
+    if cache_call:
+        _GET_NN_LEN_AT_BASE[(n, radix)] = res
+    return res
 
 
-def to_base(n: int, radix: Radix) -> str:
+def get_ith_last_digit_of_number_repr(n: int, r: Radix, i: int) -> int:
+    if i > (base_repr_len := get_nn_len_at_base(n, r)):
+        raise ValueError(
+            f"Representation of {n=:_} at base {r=} is {base_repr_len},"
+            f"whereas {i}-th digit from the ends has been requested! "
+        )
+    return n % r**i
+
+
+def to_base(n: int, r: Radix) -> str:
     _validate_is_nn(n)
-    _validate_radix(radix)
+    _validate_radix(r)
     str_rep: list[str] = []
     base_power_counter: int = 1
-    base_power_bound: int = get_nn_len_at_base(n, radix)
+    base_power_bound: int = get_nn_len_at_base(n, r)
     num: int = n
     while base_power_counter < base_power_bound:
-        remainder: int = num % radix
+        remainder: int = num % r
         str_rep.append(REMAINDER_TO_SYMBOL_MAP[remainder])
-        num = num // radix
+        num = num // r
         base_power_counter += 1
     return "".join(str_rep[::-1])

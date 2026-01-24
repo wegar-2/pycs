3b88a59e25167f1793c23c42642691c161654280
wegar2 <awegrzyn17@gmail.com>
2026-01-24 14:44:11 +0100

    added bug fix in counting_sort

--- pycs/algorithms/sorting/counting_sort.py
+++ pycs/algorithms/sorting/counting_sort.py
@@ -4,8 +4,9 @@ __all__ = ["counting_sort"]
 
 
 def _validate_input(nums: list[int]) -> None:
-    if not all(isinstance(x, int) and x >=0 for x in nums):
-        raise ValueError(f"")
+    if not all(isinstance(x, int) and x >= 0 for x in nums):
+        raise ValueError("Not all elements of input to counting_sort "
+                         "are nonnegative integers! ")
 
 
 def _max(nums: list[int]) -> int:
@@ -25,16 +26,30 @@ def counting_sort(nums: list[int], limit_multiplier: int = 100):
     if m > limit_multiplier * len(nums):
         pass
 
-    b_arr: array = array("i", [0] * m)
-    c_arr: array = array("i", [0] * len(nums))
+    counts_arr: array = array("i", [0] * (m + 1))
+    sorted_arr: array = array("i", [0] * len(nums_ar))
 
-    for x in nums:
-        b_arr[x] += 1
-    for i in range(1, len(b_arr)):
-        b_arr[i] = b_arr[i-1] + b_arr[i]
+    for x in nums_ar:
+        counts_arr[x] += 1
+    for i in range(1, len(counts_arr)):
+        counts_arr[i] = counts_arr[i-1] + counts_arr[i]
 
     for x in nums_ar[::-1]:
-        c_arr[b_arr[x]] = x
-        b_arr[x] -= 1
+        sorted_arr[counts_arr[x]-1] = x
+        counts_arr[x] -= 1
 
-    return c_arr
+    return sorted_arr
+
+
+if __name__ == "__main__":
+    import random
+
+
+    def _generate_nums(len_: int = 10_000, upper_bound: int = 1_000) -> list[
+        int]:
+        random.seed(123)
+        return [random.randint(a=1, b=upper_bound) for _ in range(len_)]
+
+
+    nums = _generate_nums()
+    assert sorted(nums) == list(counting_sort(nums))
---
+++ tests/algorithms/sorting/test_counting_sort.py
@@ -0,0 +1,24 @@
+from pycs.algorithms.sorting.counting_sort import counting_sort
+
+import random
+
+from pytest import raises
+
+def _generate_nums(len_: int = 10_000, upper_bound: int = 1_000) -> list[int]:
+    random.seed(123)
+    return [random.randint(a=1, b=upper_bound) for _ in range(len_)]
+
+
+def test_counting_sort():
+    nums = _generate_nums()
+    assert sorted(nums) == list(counting_sort(nums))
+
+
+# def test_raises_error_on_negative_numbers():
+#     with raises():
+#         pass
+#
+#
+# def test_input_too_large_elements():
+#     with raises():
+#         pass

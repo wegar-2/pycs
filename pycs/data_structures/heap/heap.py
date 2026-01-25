e348e79e0b9090ff140a0bb4b106d00690614897
wegar2 <awegrzyn17@gmail.com>
2026-01-25 21:53:23 +0100

    added draft untested version of heap

--- pycs/data_structures/heap/heap.py
+++ pycs/data_structures/heap/heap.py
@@ -5,27 +5,87 @@ class Heap:
     def __init__(self):
         self._data: list[int] = []
 
+    @property
+    def data(self) -> list[int]:
+        return self._data
+
     def __len__(self) -> int:
         return len(self._data)
 
-    def _bubble_up(self):
-        pass
+    def _down_heap_bubble(self, idx: int = 0) -> None:
+        curr_idx: int = idx
+        value: int = self._data[curr_idx]
+
+        lc_idx = self._index_left_child(idx=curr_idx)
+        rc_idx = self._index_right_child(idx=curr_idx)
+
+        if lc_idx <= len(self) - 1:
+            lc_value = self._data[lc_idx]
+            if lc_value < value:
+                self._data[lc_idx], self._data[curr_idx] = (
+                    self._data[curr_idx], self._data[lc_idx])
+                self._down_heap_bubble(idx=lc_idx)
+            else:
+                return None
+
+        if rc_idx <= len(self) - 1:
+            rc_value = self._data[rc_idx]
+            if rc_value < value:
+                self._data[rc_idx], self._data[curr_idx] = (
+                    self._data[curr_idx], self._data[rc_idx])
+                self._down_heap_bubble(idx=rc_idx)
+            else:
+                return None
+
+        return None
+
+    def _up_heap_bubble(self):
+
+        last_idx: int = len(self._data) - 1
+        curr_idx: int = last_idx
+        value: int = self._data[curr_idx]
+
+        while curr_idx >= 0:
+            parent_idx = (curr_idx - 1) // 2
+            if self._data[parent_idx] > value:
+                self._data[parent_idx], self._data[curr_idx] = (
+                    self._data[curr_idx], self._data[parent_idx])
+                curr_idx = parent_idx
+            else:
+                break
 
     def append(self, x: int) -> None:
         self._data.append(x)
+        self._up_heap_bubble()
 
-    @property
+    def pop(self) -> int:
+        if len(self) == 0:
+            ValueError("Heap is empty! ")
+        last: int = self._data.pop()
+
+        if len(self._data) == 0:
+            return last
+
+        first: int = self._data[0]
+        self._data[0] = last
+        self._down_heap_bubble()
+        return first
+
     def peek_min(self) -> int:
+        if len(self) == 0:
+            raise ValueError("Heap is empty! ")
         return self._data[0]
 
-    def pop_min(self) -> int:
-        pass
+    @staticmethod
+    def _index_parent(idx: int) -> int | None:
+        if idx == 0:
+            return None
+        return (idx - 1) // 2
 
-    def _index_parent(self, idx: int) -> int | None:
-        pass
+    @staticmethod
+    def _index_left_child(idx: int) -> int:
+        return 2 * idx + 1
 
-    def _index_left_child(self, idx: int) -> int:
-        pass
-
-    def _index_right_child(self, idx: int) -> int:
-        pass
+    @staticmethod
+    def _index_right_child(idx: int) -> int:
+        return 2 * idx + 2
---
+++ tests/data_structures/heap/heap.py
@@ -0,0 +1,17 @@
+from pycs.data_structures.heap.heap import Heap
+
+
+def test_heap_construction():
+    pass
+
+
+def test_heap_pop():
+    pass
+
+
+def test_heap_append():
+    pass
+
+
+def test_heap_peek_min():
+    pass

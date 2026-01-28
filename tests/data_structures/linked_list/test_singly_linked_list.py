58fdb200a6a48df36445f5024dd16ab963def653
wegar2 <awegrzyn17@gmail.com>
2026-01-28 18:53:52 +0100

    added implementation for some of the unit test cases

--- tests/data_structures/linked_list/test_singly_linked_list.py
+++ tests/data_structures/linked_list/test_singly_linked_list.py
@@ -1,4 +1,4 @@
-from pytest import fixture
+from pytest import fixture, raises
 
 from pycs.data_structures.linked_list.linked_list_node import LinkedListNode
 from pycs.data_structures.linked_list.singly_linked_list import (
@@ -17,33 +17,56 @@ def default_sll() -> SinglyLinkedList:
 
 
 def test_init():
+    pass
+
+
+
+def test_first_last_node_and_len():
+
     sll = SinglyLinkedList()
     assert len(sll) == 0
     first = LinkedListNode(key=22)
+
     sll.append(first)
     assert len(sll) == 1
     assert sll.first_node == first
     assert sll.last_node == first
+
     second = LinkedListNode(key=333)
     sll.append(second)
     assert len(sll) == 2
-    assert
+    assert sll.first_node == first
+    assert sll.last_node == second
+
+    last = LinkedListNode(444)
+    sll.append(last)
+    assert len(sll) == 3
+    assert sll.first_node == first
+    assert sll.last_node == last
 
 
-def test_first_last_node():
-    pass
+def test_append(default_sll):
+    new_node = LinkedListNode(4444)
+    default_sll.append(new_node)
+    assert len(default_sll) == 4
+    assert default_sll.last_node == new_node
 
 
-def test_list_len():
-    pass
+def test_pop_left(default_sll):
 
+    first_node = default_sll.pop_left()
+    assert len(default_sll) == 2
+    assert first_node.key == 1
 
-def test_append():
-    pass
+    mid_node = default_sll.pop_left()
+    assert len(default_sll) == 1
+    assert mid_node.key == 22
 
+    last_node = default_sll.pop_left()
+    assert len(default_sll) == 0
+    assert last_node.key == 333
 
-def test_pop_left():
-    pass
+    assert default_sll.pop_left() is None
 
 
 def test_pop_right():

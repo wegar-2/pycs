79b3e5d6e7ae9ca892f4f5d925fe09afbfdb04c5
wegar2 <awegrzyn17@gmail.com>
2026-02-25 17:59:15 +0100

    minor progress on implementation of finite automaton

--- pycs/automata/finite_automaton.py
+++ pycs/automata/finite_automaton.py
@@ -2,15 +2,46 @@ from typing import Final
 
 __all__ = ["FiniteAutomaton"]
 
+import pandas as pd
+
 
 class FiniteAutomaton:
 
-    def __init__(self, states: set[str]):
+    def __init__(
+            self,
+            states: set[str],
+            transition_func: pd.DataFrame,
+            input_alphabet: set[str],
+            accepting_states: set[str]
+    ):
         self._states: Final[set[str]] = states
+        self._accepting_states: Final[set[str]] = accepting_states
+        self._input_alphabet: Final[set[str]] = input_alphabet
+
+        self._validate_transition_func(transition_func)
+        self._transition_func: Final[pd.DataFrame] = transition_func
+
+    def _validate_accepting_states(self, states: set[str]) -> None:
+        if self._states.intersection(states) != states:
+            raise ValueError()
+
+    def _validate_transition_func(
+            self,
+            transition_func: pd.DataFrame
+    ) -> None:
+        pass
 
     @property
     def states(self) -> set[str]:
         return self._states
 
-    def parse(self, inp: str) -> str:
+    @property
+    def accepting_states(self) -> set[str]:
+        return self._accepting_states
+
+    @property
+    def input_alphabet(self) -> set[str]:
+        return self._input_alphabet
+
+    def parse(self, inp: list[str]) -> str:
         pass
